65cffac9b8e4b9cb5cd1133bdd1cf4a76b595c45
Sebastiaan Scholten <j.s.m.scholten@student.tudelft.nl>
2019-11-08 14:02:28 +0100

    final commit

--- seb/Create_corrplot.R
+++
@@ -1,5 +0,0 @@
-library(GGally)
-
-ggpairs(combined_cast_year[2:7], aes())
-# need to attach variable IncomeGroup in combined cast
-# toevoegen: aes(colour = IncomeGroup, alpha = 0.4)
--- seb/example models/17_linear_regress.R
+++
@@ -1,5 +0,0 @@
-broadband <- read.table("data/broadband_access1.csv",header=TRUE,sep=",")
-
-linearMod <- lm(bbnd ~ urb, data=broadband)  # build linear regression model on full data
-print(linearMod)
-summary(linearMod)
