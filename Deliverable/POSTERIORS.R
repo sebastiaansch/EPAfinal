c873a7cb37c16a4d8d1d66467f29280bfc9eb63c
Sebastiaan Scholten <j.s.m.scholten@student.tudelft.nl>
2019-11-08 18:13:25 +0100

    final push

--- Deliverable/POSTERIORS.R
+++ Deliverable/POSTERIORS.R
@@ -1,18 +1,18 @@
 library(scales)
-#the data consists of total broadband accounts across 93 countries
-broadband = read.table("broadband_access1.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
-code <- broadband$Country.Code
-urb <- log(broadband$AG.LND.TOTL.UR.K2)
-n <- length(urb)
+#43 countries
+dataset = read.table("testdata.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
+code <- dataset$Country.Code
+PRM <- log(dataset$School.enrollment..primary....net.)
+n <- length(PRM)
 
 intercept <- rep(1,n)
 
 # 200 runs by 2 parameters
-model_runs = read.table("noisymodelruns.csv", sep = ",", header = TRUE)
+model_runs = read.table("data/modelruns.csv", sep = ",", header = TRUE)
 model_runs <- as.matrix(model_runs)
 
-# 2 parameters by 93 nations
-design_matrix <- t(cbind(intercept, urb))
+# 2 parameters by 43 nations
+design_matrix <- t(cbind(intercept, PRM))
 
 p <- model_runs %*% design_matrix
 posteriors <- as.data.frame(p)
